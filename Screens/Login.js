ca8936a9f364d494114a61ec3838da9ecf42b197
Sagar Teria <sagarteria@gmail.com>
2023-08-24 14:24:36 +0000

    added google login code

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+WebBrowser.maybeCompleteAuthSession();
 
 const LoginPage = ({navigation}) => {
+
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: "917856360906-e4sgjeav3iiftdjae4db0ctpueres5ec.apps.googleusercontent.com",
+    androidClientId: "917856360906-roaddemnd0fn03vhn467uq12ei2bt01e.apps.googleusercontent.com",
+    iosClientId: "917856360906-e4e8omhl85kbq1mikc23o6l9uqu0n7n1.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    handleEffect();
+  }, [response, token]);
+
+  async function handleEffect() {
+    const user = await getLocalUser();
+    console.log("user--", user);
+    if (!user) {
+      if (response?.type === "success") {
+        console.log("user--", user, response);
+        // setToken(response.authentication.accessToken);
+        getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(user);
+      console.log("loaded locally");
+      navigation.navigate('MainApp');
+    }
+  }
+
+  const getLocalUser = async () => {
+    const data = await AsyncStorage.getItem("@user");
+    if (!data) return null;
+    return JSON.parse(data);
+  };
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+      navigation.navigate('MainApp');
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
   const handleGoogleLogin = () => {
     navigation.navigate('MainApp');
   };
@@ -17,7 +77,9 @@ const LoginPage = ({navigation}) => {
         <Text style={styles.logoText}>Travel Push</Text>
       </View>
       <View style={styles.loginButtonContainer}>
-        <TouchableOpacity style={styles.googleLoginButton} onPress={handleGoogleLogin}>
+        <TouchableOpacity style={styles.googleLoginButton} onPress={() => {
+            promptAsync();
+          }}>
           <Image
             source={require('../assets/google-login-icon.png')}
             style={styles.googleIcon}
